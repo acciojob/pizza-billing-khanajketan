58b79cc9d896502693f6c7d52725f0a7e78f79bf
Ketan Khanaj <khanajketan@gmail.com>
2023-04-20 17:08:40 +0530

    1st pizza commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,9 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
-        // your code goes here
+        super.setPrice(450);
+        super.isDeluxe = true;
+        super.setBill("Veg Deluxe pizza price : 450");
+
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,68 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int price = 300;
+    public boolean isDeluxe = false;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     private Boolean isVeg;
     private String bill;
 
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+        if(isVeg == false) this.price = this.price + 50;
         // your code goes here
     }
 
     public int getPrice(){
         return this.price;
     }
-
+    boolean flag1 = true;
     public void addExtraCheese(){
         // your code goes here
-    }
 
+        if(flag1 && !isDeluxe){
+            this.price = this.price + 80;
+            this.bill = bill + "\nExtra Cheese Price : 80";
+            flag1 = false;
+        }
+
+    }
+    boolean flag2 = true;
     public void addExtraToppings(){
         // your code goes here
-    }
 
+        if(flag2 && !isDeluxe){
+            this.price = this.price + 70;
+            if(isVeg){
+                this.bill = bill +"\nExtra Toppings For Veg Pizza : 70";
+            }else{
+                this.bill = bill +"\nExtra Toppings For Non-veg Pizza : 120";
+            }
+            flag2 = false;
+        }
+    }
+    boolean flag3 = true;
     public void addTakeaway(){
         // your code goes here
+
+        if(flag3){
+            this.price = this.price + 20;
+            this.bill = bill +"\nPaper bag Price : 20";
+            flag3 = false;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        this.bill = this.bill + "\nTotal Price : "+ price;
         return this.bill;
     }
 }
